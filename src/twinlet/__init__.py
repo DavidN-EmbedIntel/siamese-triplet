393da8ccd1bc183734e1ddada962e3bb6cd6daca
David Nicholson <david@embedintel.com>
2020-07-27 20:57:36 -0400

    add src/twinlet package, move modules into it

---
+++ src/twinlet/__init__.py
@@ -0,0 +1,15 @@
+from . import datasets
+from . import losses
+from . import metrics
+from . import networks
+from . import trainer
+from . import utils
+
+__all__ = [
+    'datasets',
+    'losses',
+    'metrics',
+    'networks',
+    'trainer',
+    'utils',
+]
